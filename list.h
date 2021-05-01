e67a00fc470cbd68479a028ac543f4716c2234f9
redstonecold <hscj01@naver.com>
2021-05-01 23:32:10 +0900

    list.h

---
+++ list.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct{
+    char date[10];
+    char name[60];
+    char attendance;
+} List;
+
+int createList(List *l);
+void readList(List *l); 
+void listList(List* l, int count);
+int updateList(List *l);   
+int deleteList(List *l);      
+
+int selectMenu();
+int selectDataNo(List* l, int count);
+
+int loadData(List *l);
+void saveData(List *l, int count);
+
+void searchDate(List *l, int count);
+void searchName(List *l, int count);
+void searchAttendance(List *l, int count);
